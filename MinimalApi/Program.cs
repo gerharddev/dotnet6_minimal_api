66f5efbed6bf14614b47e7283eceaa1760cc321d
Gerhard Maree <Gerhardus.Maree@sms-digital.com>
2021-10-11 18:03:03 +0200

    Empty Api Swagger support

---
+++ MinimalApi/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+app.UseSwagger();
+app.MapGet("/", () => "Hello World!");
+app.UseSwaggerUI();
+
+app.Run();

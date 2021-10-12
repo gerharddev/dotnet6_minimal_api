667e68d5b20b06ca19b201b1776a37642e758769
Gerhard Maree <Gerhardus.Maree@sms-digital.com>
2021-10-12 08:02:08 +0200

    Get & Post - no validation

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,13 +1,42 @@
-﻿
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApi;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<UserDbContext>(options => options.UseInMemoryDatabase("Users"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.UseSwagger();
-app.MapGet("/", () => "Hello World!");
+app.MapSwagger();
 app.UseSwaggerUI();
 
+app.MapGet("/users", async (UserDbContext context) =>
+await context.Users.ToListAsync())
+.WithName("GetAllUsers");
+
+app.MapGet("/users/{id}", async (int id,UserDbContext context) =>
+await context.Users.FindAsync(id)
+    is User user
+        ? Results.Ok(user)
+        : Results.NotFound())
+.WithName("GetUserById")
+.Produces<User>(StatusCodes.Status200OK)
+.Produces(StatusCodes.Status404NotFound);
+
+app.MapPost("/users", async (User user, UserDbContext context) =>
+{
+
+    context.Users.Add(user);
+    await context.SaveChangesAsync();
+
+    return Results.Created($"/users/{user.Id}", user);
+})
+.WithName("PostUser")
+.ProducesValidationProblem()
+.Produces<User>(StatusCodes.Status201Created);
+
 app.Run();
+
+
---
+++ MinimalApi/User.cs
@@ -0,0 +1,16 @@
+﻿namespace MinimalApi
+{
+    public class User
+    {
+        public User(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
